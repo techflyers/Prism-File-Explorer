33e5cfc5b290e4cf96fe436a7c264c1bc918f0eb
techflyers <techflyermail+git@gmail.com>
2026-08-26 19:27:18 +0530

    chore: bump version code to 13 and version name to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.raival.compose.file.explorer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.1.1"
     }
 
     dependenciesInfo {
